e485efbd1d2e19225fc7c6b267b51ca3b56024dc
thalesaraujo16 <thalesaraujo1609@gmail.com>
2025-12-28 23:16:18 +0000

    fix pomo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const basePath = mode === 'development' ? '/' : (env.GH_PAGES_BASE || '/pomodoro-teste/');
     return {
+      base: basePath,
       server: {
         port: 3000,
         host: '0.0.0.0',
